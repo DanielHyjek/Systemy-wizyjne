462770cbc098ebdd9ee9c4b40a3397fbcbe26083
Daniel Hyjek <daniel.hyjek@gmail.com>
2017-05-15 23:45:43 +0200

    lab9 niby dziala ale slabe to

--- lab9 segmentacja/split.m
+++ lab9 segmentacja/split.m
@@ -1,23 +1,21 @@
-function split(obraz,x,y,X,Y)
-global segRes;
-global segMean;
-global prog;
-global minimum;
-global index;
+function [] = split(image, x1, y1, x2, y2)
 
-obszar=obraz(x:X,y:Y);
-srednia=mean(obszar(:));
-odchylenie=std(obszar(:));
+global segRes segMean minimum index prog
 
-if (odchylenie>prog && (X-x) >= minimum(1,1) && (Y-y) >= minimum(1,2))
-    O=round([(X-x)/2 (Y-y)/2])
-    split(obraz,x,y,O(1),O(2));
-    split(obraz,O(1),y,X,O(2));
-    split(obraz,x,O(2),O(1),Y);
-    split(obraz,O(1),O(2),X,Y);
-else
-    index=index+1;
-    segRes(x:X,y:Y) = index; 
-    segMean(x:X,y:Y) = srednia; 
-end
+temp = image(x1:x2, y1:y2);
+[row, col]=size(temp);
+srednia = mean(double(temp(:)));
+odchylenie = std(double(temp(:)));
+
+    if odchylenie > prog && row > minimum(1,1) && col > minimum(1,2)
+        split(image, x1, y1, round((x2-x1)/2)+x1, round((y2-y1)/2)+y1);
+        split(image, x1, round((y2-y1)/2)+y1, round((x2-x1)/2)+x1, y2);
+        split(image, round((x2-x1)/2)+x1, y1, x2, round((y2-y1)/2)+y1);
+        split(image, round((x2-x1)/2)+x1, round((y2-y1)/2)+y1, x2, y2);
+    else 
+        index = index + 1;
+        segRes(x1:x2, y1:y2) = index;
+        segMean(x1:x2, y1:y2) = srednia;
+    end
+
 end
--- lab9 segmentacja/zad1.m
+++ lab9 segmentacja/zad1.m
@@ -1,20 +1,53 @@
 close all, clear all;
 
 obraz_org=imread('umbrealla.png');
-obraz=rgb2hsv(obraz_org);
+obraz=double(rgb2hsv(obraz_org));
+image=obraz(:,:,1);
 
-rozm=size(obraz_org);
-global segRes;
-global segMean;
-global prog;
-global minimum;
-global index;
+rozm=size(image);
+global segRes segMean prog minimum index;
 index=1;
-minimum=[8 8];
-prog = 0.05;
+minimum=[4 4];
+prog = 0.02;
 segRes = ones(rozm(1,1),rozm(1,2));
 segMean = ones(rozm(1,1),rozm(1,2));
+prog2=0.02;
 
 
-split(obraz,1,1,256,256);
-imshow(segRes,[])
+split(image,1,1,256,256);
+imshow(segMean)
+
+for i_index = 1:index
+    
+    IB = segRes == i_index;
+    IBDilate = imdilate(IB,strel('square',3));
+    IBDiff = IBDilate - IB;
+    IBMult = IBDiff.*segRes;
+    IBMultNZ = nonzeros(IBMult);
+    IBUnique = unique(IBMultNZ);
+    
+    for i = 1:length(IBUnique)
+        IBS = segRes == IBUnique(i);
+        [y_IBS , x_IBS] = find(IBS, 1 , 'first');
+        [y_IB, x_IB] = find(IB, 1, 'first');
+        if abs(segMean(y_IBS, x_IBS) - segMean(y_IB, x_IB)) < prog2
+            segRes(IBS) = i_index;
+        end
+    end
+end
+
+% Zerowaie ma³ych obszarów
+ind=unique(segRes);
+for i = 1:length(ind)
+    IB = segRes == i;
+    pole=sum(IB);
+    if pole<120
+        segRes=segRes.*~IB;
+    end
+end
+
+
+
+figure
+imshow(label2rgb(segRes), []);
+title('Segmented')
